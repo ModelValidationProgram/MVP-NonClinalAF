3770e8b74d83bdb1fe409a566f129a5b7f65e3db
Katie Lotterhos <k.lotterhos@gmail.com>
2021-06-17 12:08:14 -0400

    notebook

---
+++ notebook/20210617_cloneRepoToDiscovery.md
@@ -0,0 +1,64 @@
+# 20210617 
+
+Cloned the github repo to Discovery
+
+First, I followed these instructions to set up a public/private key connection between GitHub and Discovery: https://docs.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh 
+
+```
+ssh lotterhos@discovery.neu.edu
+# signed in
+cd work/lotterhos
+git clone git@github.com:ModelValidationProgram/MVP-NonClinalAF.git
+
+git branch alan
+git checkout alan
+git commit -a -m "add branch"
+```
+
+## To do for Alan:
+- [ ] make sure can access work/lotterhos/MVP-NonClinalAF
+- [ ] make sure can link github account to discovery and make changes on `alan` branch
+- [ ] don't "F" up using github - review the "git" chapter in Buffalo's book and code below
+- [ ] review the command line script in `run_nonAF_sims_alan.sh`. 
+- [ ] Create a conda environment for running the shell script
+    - [ ] update the notebook and push to github
+- [ ] test the bash script gives proper output, in particular the VCF editing line
+    - [ ] update the notebook and push to github
+- [ ] Create a bash script for running 1 replicate of the simulation as shown in the bash script
+    - [ ] update the notebook and push to github
+- [ ] review the code in `0c-writeSimScript.R` that creates some of the scripts
+- [ ] Write an array to run the first 150 lines (parameter combos) of the `0b-final_params.txt`
+    - [ ] update the notebook and push script to github. Note here there are some simulation outputs that are not pushed to github because they are so big. So we should review those before you try to push the outputs too.
+
+#### Step 1: get off the login node
+
+srun
+
+#### Step 2 view the branches:
+
+`git branch` 
+
+alan
+
+* main
+
+#### Swith to branch `alan`:
+
+`git checkout alan` 
+
+Switched to branch 'alan'
+
+
+#### Tracking files
+
+Let’s use git add to tell Git to track the files:
+* `git add README data/README` will track both README in the current dir and data/README
+* `git add --all sim_outputs1/` adds everything in the folder
+
+`git status` tells us what is staged to be committed
+
+after adding everything, we commit it `git commit -a -m "reogoranize"`
+
+then you can push `git push origin alan` for alan's branch, or  `git push origin main` for the main branch
+
+Once we are happy with changes on alan's branch, we will submit a merge request with the main branch
--- src/run_nonAF_sims copy.sh
+++ src/run_nonAF_sims copy.sh
@@ -3,55 +3,48 @@ set -e
 set -u
 set -o pipefail
 
-mypath="/Users/lotterhos/Documents/GitHub/MVP-NonClinalAF/src"
+mypath="/Users/lotterhos/Documents/GitHub/MVP-NonClinalAF"
 cd $mypath
-nreps=210 #number of reps
-start=10900  #10900 10950
-finish=$(($start + $nreps-1))
-echo -e $start $finish
 
-i = 2242330863068
+outpath="sim_outputs/"
+i=1231094 #this is MY_SEED
 
-for i in $(seq $start $finish)
-do
-    cd $mypath
-    echo -e "\n\n"
-    echo $i
-	outfile=${i}"_out.txt"
-	outpath="../sim_outputs/"
-	outpathfile=${outpath}${outfile}
-    #echo $outpathfile
-
+    cat > sim_outputs/1231094__oliogenic_1-trait__Est-Clines_N-cline-center-to-edge_m-constant.txt
+    
     ##############
     #### run slim sims (takes 10 min)
     #############
     echo "running SLiM"
     SECONDS=0 # used to time analyses, no spaces around "=" sign
 	#run slim in background
-	slim -d seed=$i -d mypath="'${outpath}'" ../src_sims/INSERTFILENAME.slim > $outpathfile 2> $outpathfile".error" & echo $!
-		# input my seed and path to outputput
-    wait
+    slim -d MY_SEED1=1231094 -d "MY_RESULTS_PATH1='sim_outputs/'" -d MIG_x1=0.01 -d MIG_y1=0.01 -d "demog1='SS'" -d "xcline1='V'" -d "ycline1='linear'" -d METAPOP_SIDE_x1=10 -d METAPOP_SIDE_y1=10 -d Nequal1=1 -d isVariableM1=0 -d MIG_breaks1=0 -d MU_base1=1e-07 -d MU_QTL_proportion1=0.01 -d SIGMA_QTN_1a=0.4 -d SIGMA_QTN_2a=0.4 -d SIGMA_K_1a=0.5 -d SIGMA_K_2a=0.5 -d N_traits1=1 -d ispleiotropy1=0 src/a-PleiotropyDemog.slim > sim_outputs/1231094_outfile.txt 2> sim_outputs/1231094_outfile.error.txt #  & echo $!
+    
+    
+    # NOTE: the "&" runs in the background on my laptop, but may not be needed on cluster
+		
+   # wait
     echo "Done with SLiM sims. Analysis took $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min"
 
-    # make sure output from last analysis exists
-    if [ ! -f "results/${i}.trees" ]; then
-        echo "trees file for ${i} not found, skipping"
-        continue # skip rest of code and go to next $i
-    fi
-    
+
     gzip -f ${outpath}${i}"_VCF_causal.vcf"
 
     ##############
-    #### run python script to process tree sequence results  (takes 1 min)
+    #### run python script to process tree sequence results
+    #### Takes overnight on my laptop ~24 hours
     #############
     cd results
     echo "processing tree sequences in python"
     SECONDS=0 # used to time analyses
-    python3 ../src/a_process_trees.py -s ${i} > ${outpath}${i}"_pytree.out" 2> ${outpath}${i}"_pytree.error" & echo $!
+    
+    # mu is MU_base
+  python3 src/b-process_trees.py -s 1231094 -r 1e-06 -mu 1e-06 -N 1000 > sim_outputs/1231094_pytree.out.txt 2> sim_outputs/1231094_pytree.error.txt
+    #python3 src/b-process_trees.py -s ${i} -r 1e-06 -mu 1e-06 -N 1000 > ${outpath}${i}"_pytree.out.txt" 2> ${outpath}${i}"_pytree.error.txt" ##& echo $!
+    # If I understand recaptitaion correctly, it applies to the point in time prior to
+    # when the SLiM simulation started, and the "N" is the N for the whole metapopulation
+    # I'll use N=10000 for all sims, since that is close to the total metapop size for all sims
+    # NOTE: the "&" runs in the background on my laptop, but may not be needed on cluster
 
-	TO DO ADD MORE PARAMETERS AS INPUT TO PROCESS TREES
-
-    wait #wait until the last background process is finished
+    #wait #wait until the last background process is finished
 
     echo "Done with processing tree sequences. Analysis took $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min"
 
@@ -68,6 +61,7 @@ do
     #sed -i 's/[.]\t\t/[.]\t0\t/g' ${outpath}${i}"_plusneut_MAF01.recode.vcf" #maybe will work in Linux
     
     sed 's/\.		/\.	0	/g' ${outpath}${i}"_plusneut_MAF01.recode.vcf" > ${outpath}${i}"_plusneut_MAF01.recode2.vcf" #this works on my mac
+    # not sure if it will work on linux
     
     gzip -f ${outpath}${i}"_plusneut_MAF01.recode2.vcf"
     
@@ -79,8 +73,8 @@ do
     ##############
     #### run R script  (takes 3 min)
     #############
-    SECONDS=0 # used to time analyses
-    echo "Running R scripts"
-    Rscript --vanilla ../src/b_Proc_Sims.R ${i} $simType > ${i}"_Invers_R.out" 2> ${i}"_Invers_R.error" & echo $!
-    wait #wait until the last background process is finished
-    echo "Done with processing first R script. Analysis took $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min"
+    #SECONDS=0 # used to time analyses
+    #echo "Running R scripts"
+    #Rscript --vanilla ../src/b_Proc_Sims.R ${i} $simType > ${i}"_Invers_R.out" 2> ${i}"_Invers_R.error" & echo $!
+    #wait #wait until the last background process is finished
+    #echo "Done with processing first R script. Analysis took $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min"

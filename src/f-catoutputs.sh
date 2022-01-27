185a62c70198c4f7863ff79c7d48dda0a88f17cd
Katie Lotterhos <k.lotterhos@neu.edu>
2022-01-27 09:49:44 +0100

    Update f-catoutputs.sh

--- src/f-catoutputs.sh
+++ src/f-catoutputs.sh
@@ -1,3 +1,15 @@
-cd ../sim_output_150_20210830/ #update folder
-awk 'NR==1' 1231094_Rout_simSummary.txt > ../summary_20210920.txt # header
-awk 'FNR>1' *_Rout_simSummary.txt >> ../summary_20210920.txt # data
+cd ../sim_output_20220117/ #update folder
+
+ls -l *_muts.txt | wc -l # this is the number of sims that completed the SliM run
+
+ls -l *_plusneut_MAF01.recode2.vcf.gz | wc -l # this is the number of sims that completed the vcf filtering
+
+ls -l ../*.pcaProject | wc -l # this is the number of sims that were analyzed through the PCA step in the R script
+
+ls -l *_Rout_simSummary.txt | wc -l # this is the number of sims that were analyzed through the final output in the R script
+
+
+
+awk 'NR==1' 1231094_Rout_simSummary.txt > ../summary_20220117.txt # header
+
+awk 'FNR>1' *_Rout_simSummary.txt >> ../summary_20220117.txt # data
